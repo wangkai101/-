7ef2de7494e1a59e40214bbb7b41406792dc3976
wangkai101 <645182907@qq.com>
2019-08-10 12:12:50 +0800

    上传

--- 弹窗/ViewController.swift
+++ 弹窗/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //@IBOutlet var alertView1: UIButton!
+    @IBAction func alertButtonClick(_ sender: UIButton) {
+        var alertView1 = UIAlertController(title: "hello", message: "hello everyone", preferredStyle: self)
+        alertView1.show()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
